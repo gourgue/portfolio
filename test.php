753fe67d5858dfb78d3e808dffb4f95ca9d0f665
gourgue <mikegourgue@gmail.com>
2018-06-08 17:22:29 +0200

    ajout du fichier test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php 
+
+function maximum($liste = [1,54,9,1,3,21,47,3,18,9]){
+
+	$maxActuel = [] ;
+	foreach ($liste as $element) {
+		if (count($element) > 1){
+		echo $element . "\n";
+	}
+}
+
+
+ maximum();
+ ?>
